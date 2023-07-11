7a99278eede3fcd42c297e3fe79d3f9ac8e1e2c1
Alex Odobesteanu <alexandru.odobesteanu@gmail.com>
2023-07-11 15:02:00 +0300

    Adding Inheritance Principles & Restructuring

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
-import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
+import { Model } from "./Model";
 
 export interface userProps {
   id?: number;
@@ -11,50 +8,4 @@ export interface userProps {
 
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<userProps> = new Sync<userProps>(rootUrl);
-  public attributes: Attributes<userProps>;
-
-  constructor(attrs: userProps) {
-    this.attributes = new Attributes<userProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: userProps): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.get("id");
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an ID");
-    }
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
-  }
-}
+export class User extends Model<userProps> {}
