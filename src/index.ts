f0860d757387c7c823c80d1d8a0eb3f604a3afea
Alex Odobesteanu <alexandru.odobesteanu@gmail.com>
2023-07-12 18:02:00 +0300

    Added event binds

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
-const userForm = new UserForm(document.getElementById("root") as HTMLElement);
+const user = User.buildUser({ name: "NAME", age: 20 });
+const userForm = new UserForm(
+  document.getElementById("root") as HTMLElement,
+  user
+);
 userForm.render();
--- src/models/User.ts
+++ src/models/User.ts
@@ -26,4 +26,9 @@ export class User extends Model<userProps> {
       User.buildUser(json)
     );
   }
+
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
+  }
 }
